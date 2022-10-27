16dffc990888b70f7a8cf5610a6e1c4f4dd3dbf6
saliha lammari <75095222+salihalammari@users.noreply.github.com>
2022-10-27 21:05:12 +0800

    parser

--- srcs/push_swap/ft_parsing.c
+++ srcs/push_swap/ft_parsing.c
@@ -10,3 +10,25 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../includes/push_swap.h"
+
+void	ps_parsing(const char *str, t_ps *a)
+{
+	int		i;
+	char	**tmp;
+
+	i = 0;
+	tmp = ft_split(str, ' ');
+	if (!tmp[i])
+		ps_error(2);
+	while (tmp[i])
+	{
+		if (!ps_right_add(a, ps_atoi(tmp[i])))
+			ps_error(2);
+		free(tmp[i]);
+		tmp[i] = NULL;
+		i++;
+	}
+	free(tmp);
+	tmp = NULL;
+}
